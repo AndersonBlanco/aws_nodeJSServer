3ed7baa70ed60c8147efba7998df2e5188465218
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-05-16 10:30:28 -0400

    server edits

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const exp = require('express');
 const app = exp();
 
 app.get('/', (req, res) =>{
-    res.send('Hello Universe Once Again'); 
-})
+   res.sendFile('./root/home.html');
+
+});
 const port = 8080;
 app.listen(port, () =>{
     console.log(`Server listening on port ${port}`)
