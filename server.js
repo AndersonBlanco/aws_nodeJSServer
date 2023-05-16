ab91928217f821ba18a8d5f038480af47b9a8804
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-05-16 10:34:04 -0400

    page edit1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const exp = require('express'); 
 const app = exp();
 
+app.use(exp.static(__dirname + '/root'));
 app.get('/', (req, res) =>{
    res.sendFile('root/home.html', {root: __dirname});
 
