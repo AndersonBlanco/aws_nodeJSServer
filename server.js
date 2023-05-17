a140da36234d703ca1647be004ab42fe2787cdc1
Anderson Blanco <andersonblanco913@gmail.com>
2023-05-16 21:49:07 -0400

    backFundamentals

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//import jsPython from 'jspython-interpreter';
+
 const exp = require('express');  
-//const { jsPython } = require('jspython-interpreter');
-//const jsPython = require('jspython-interpreter');
 
 const app = exp();
 
 const script = `print('Hello Universe')`;
-//let scriptRes = jsPython().evaluate(script).then( r => r,e => e);
+
 app.use(exp.static(__dirname + '/root'));
 app.get('/', (req, res) =>{
    res.sendFile('root/home.html', {root: __dirname});
-   //res.send(scriptRes)
+
 });
 const port = 8080;
 app.listen(port, () =>{
