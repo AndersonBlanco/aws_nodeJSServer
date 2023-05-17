3ba49f025692cbb6f7ff0650f190740c0c838961
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-05-16 23:35:31 -0400

    a

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,3 +1,1 @@
-let jsPython = require('jspython-interpreter');
-let script = `print('Hello Universe')`;
-const res = jsPython.jsPython().evaluate(script).then(r => alert(r), e => alert(e));
+alert('srnfjk')
