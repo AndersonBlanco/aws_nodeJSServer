08064da839db33a0bf229b0954793e0d911f3eb3
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-05-17 00:08:25 -0400

    a

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -2,11 +2,11 @@ const button = document.getElementById('signInButton');
 const passwordInput = document.getElementById('passwordInput');
 const pinInput = document.getElementById('pinInput');
 
-let password = '';
+let password = passwordInput.value;
 let pin = '';
 
-passwordInput.onchange = (v) => password = v; 
+//passwordInput.onfocus = (v) => password =v; 
 pinInput.onchange = (v) => pin = v; 
 button.onclick = () =>{
- alert(password);
+ alert(password)
 }
