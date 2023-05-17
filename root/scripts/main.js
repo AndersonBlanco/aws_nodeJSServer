5390ffed142b6dd67db26f81a07c386cdec5d2cd
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-05-17 00:03:00 -0400

    a

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,4 +1,12 @@
 const button = document.getElementById('signInButton');
+const passwordInput = document.getElementById('passwordInput');
+const pinInput = document.getElementById('pinInput');
+
+let password = '';
+let pin = '';
+
+passwordInput.onchange = (v) => password = v; 
+pinInput.onchange = (v) => pin = v; 
 button.onclick = () =>{
-    alert("Hello Univers"); 
+ alert(password);
 }
